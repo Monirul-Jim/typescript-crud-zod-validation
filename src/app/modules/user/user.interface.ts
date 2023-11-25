badc912a89a65bc412065cfa891582d67bb5ec15
unknown <Monirul-Jim>
2023-11-25 19:33:29 +0600

    api hit and delete user

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,26 +1,22 @@
 import { Model } from 'mongoose';
 
-// Type for User Full Name
 export type TFullName = {
   firstName: string;
   lastName: string;
 };
 
-// Type for User Address
 export type TAddress = {
   street: string;
   city: string;
   country: string;
 };
 
-// Type for User Order
 export type TOrder = {
   productName: string;
   price: number;
   quantity: number;
 };
 
-// Type for User
 export type IUser = {
   userId: number;
   username: string;
@@ -33,8 +29,6 @@ export type IUser = {
   address: TAddress;
   orders?: TOrder[];
 };
-
-// Custom Static Methods for User
 export interface UserModel extends Model<IUser> {
   isUserExist(userId: number): Promise<IUser | null>;
 }
