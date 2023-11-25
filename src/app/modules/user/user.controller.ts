b1f424c3f7aa74da1a80ccbdfb89a6c50a4172a6
unknown <Monirul-Jim>
2023-11-25 19:29:36 +0600

    post route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import {
   addOrder,
@@ -12,34 +11,23 @@ import {
 } from './user.service';
 import { validateUser } from './user.validation';
 
-// Controller function for creating a new user
 export const createUsers = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    // Get the user data from the request body
     const user = req.body;
-
-    //   Check if the user.hobbies is an array. If not, make it an array.
     if (!Array.isArray(user.hobbies)) {
       user.hobbies = [user.hobbies];
     }
-
-    // Validate the incoming user data
     const validatedUser = validateUser(user);
-
-    // Create the user using the service function
     const newUser = await createUser(validatedUser);
-
-    // Send the response
     res.status(201).json({
       success: true,
       message: 'User created successfully!',
       data: newUser,
     });
   } catch (error: any) {
-    // Handle errors, send an appropriate response
     res.status(400).json({
       success: false,
       message: 'Failed to create user!',
@@ -51,23 +39,18 @@ export const createUsers = async (
   }
 };
 
-// Controller function for getting all users
 export const getUsersController = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    // Get all users using the service function
     const users = await getAllUsers();
-
-    // Send the response
     res.status(200).json({
       success: true,
       message: 'Users fetched successfully!',
       data: users,
     });
   } catch (error: any) {
-    // Handle errors, send an appropriate response
     res.status(400).json({
       success: false,
       message: 'Failed to fetch users!',
@@ -79,26 +62,19 @@ export const getUsersController = async (
   }
 };
 
-// Controller function for getting a user by id
 export const userByIdController = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    // Get the user id from the request params
     const { userId } = req.params;
-
-    // Get the user using the service function
     const user = await getUserById(userId);
-
-    // Send the response
     res.status(200).json({
       success: true,
       message: 'User fetched successfully!',
       data: user,
     });
   } catch (error: any) {
-    // Handle errors, send an appropriate response
     res.status(400).json({
       success: false,
       message: 'Failed to fetch user!',
@@ -110,13 +86,11 @@ export const userByIdController = async (
   }
 };
 
-// Controller function for updating a user by userId
 export const updateAUser = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    // Get the user id from the request params
     const { userId } = req.params;
 
     // Get the user data from the request body
@@ -179,10 +153,7 @@ export const deleteSingleUser = async (
 };
 
 // Controller function for adding an order to a user
-export const addOrders = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
+export const addOrders = async (req: Request, res: Response): Promise<void> => {
   try {
     // Get the user id from the request params
     const { userId } = req.params;
