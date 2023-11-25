fb42f009dacd29ad12ac0ba3f2e28bbe0d7c7eb6
unknown <Monirul-Jim>
2023-11-25 19:34:09 +0600

    api hit and modify user info

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -9,27 +9,23 @@ import {
 import bcrypt from 'bcrypt';
 import config from '../../config';
 
-// MongoDB schema for the full name
 const fullNameSchema: Schema<TFullName> = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
 });
 
-// MongoDB schema for the address
 const addressSchema: Schema<TAddress> = new Schema({
   street: { type: String, required: true },
   city: { type: String, required: true },
   country: { type: String, required: true },
 });
 
-// Mongoose schema for the order
 const orderSchema: Schema<TOrder> = new Schema({
   productName: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
 });
 
-// Mongoose schema for the user
 const userSchema: Schema<IUser, UserModel> = new Schema({
   userId: {
     type: Number,
@@ -86,9 +82,7 @@ const userSchema: Schema<IUser, UserModel> = new Schema({
   },
 });
 
-// pre hook for the user schema
 userSchema.pre<IUser>('save', async function (next) {
-  //   Making password hash
   const user = this;
   user.password = await bcrypt.hash(
     user.password,
@@ -97,7 +91,6 @@ userSchema.pre<IUser>('save', async function (next) {
   next();
 });
 
-// Post hook for the user schema
 userSchema.post('save', async function (doc, next) {
   const user = await User.findOne(doc._id).select('-password -orders');
   if (user) {
